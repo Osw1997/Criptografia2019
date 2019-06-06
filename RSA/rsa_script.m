44fa9dcd91f29c017465b7ccf5c6343b6d46e90a
PAEZ ORTEGA OSWALDO EMMANUEL <zacate97@hotmail.com>
2019-06-05 19:46:37 -0500

    RSA :+1:
    Última práctica de criptografía 2019

--- RSA/rsa_script.m
+++ RSA/rsa_script.m
@@ -45,33 +45,57 @@ d = res_euclides;
 n;
 
 % Diccionario de alfabeto y símbolos
-dicc = 1:54;
 dicc_str = num2str(dicc');
-dicc_let = ['A':'Z' '0':'9' 'ÁÉÍÓÚ .,¿?¡!";Ü'];
+% dicc_let = ['A':'N' 'Ñ':'Z' '0':'9' 'ÁÉÍÓÚ .,¿?¡!";Ü'];
+dicc_let = ['A':'N' 'Ñ' 'O':'Z' '0':'9' 'ÁÉÍÓÚ .,¿?¡!";Ü'];
 % se agregan ceros del 1 al 9
 for m =1 : 9
     dicc_str(m, 1:2) = ['0' dicc_str(m, 2)];
 end
-msj = 'Hola crayola ü';
+
+% Texto a cifrar
+id = fopen('texto.txt', 'r');
+msj = upper(fscanf(id, '%c'));
+fclose(id);
+% msj = 'Hola crayola ü';
+
+% msj = 'Hola 12345';
 msj_upper = upper(msj);
 long_msj = length(msj_upper);
-
-array_rsa = zeros(1,long_msj);
-for m = 1:long_msj
-%     array_rsa(m) = uint64(mod(find(msj_upper(m) == dicc_let)^e,n));
-%     array_rsa(m) = mod(find(msj_upper(m) == dicc_let)^e,n);
-    array_rsa(m) = powermod(find(msj_upper(m) == dicc_let), e, n);
+if (mod(long_msj, 5) ~= 0)
+    msj_upper = [msj_upper repmat(' ', [1,mod(long_msj, 5)])];
 end
 
-% % Descifrado
-array_rsa_des = zeros(1,long_msj);
-msj_final = zeros(1,long_msj);
-for m = 1:long_msj
-%     array_rsa_des(m) = uint64(mod(array_rsa(m)^d, n));
-%     array_rsa_des(m) = mod(array_rsa(m)^d, n);
-    array_rsa_des(m) = powermod(array_rsa(m), d, n);
-    msj_final(m) = dicc_let(array_rsa_des(m));
+
+% Se divide el mensaje en bloques de 5 (número de 10 dígitos, 2 por c/letra)
+filas = ceil(long_msj/5);
+bloque = zeros(filas, 5);
+indx = 1;
+for m = 1:filas
+    bloque(m, 1:5) = msj_upper(indx : indx + 4);
+    indx = indx + 5;
 end
-msj_final = char(msj_final)
+% Se obtienen los números de 10 cifras de cada bloque
+numeros_10 = zeros(filas,10);
+for x = 1:filas
+    array_10 = [];
+    for y = 1:5
+        array_10 = [array_10 dicc_str(find(dicc_let == char(bloque(x,y))),1:2)];
+    end
+    array_10
+%     pause;
+    numeros_10(x, 1:10) = str2num(array_10);
+end
 
 
+% Se guarda el resultado en un txt
+id = fopen('cif_rsa.txt','wt');
+% array_rsa = zeros(1,long_msj);
+array_rsa = zeros(1,filas);
+% for m = 1:long_msj
+for m = 1:filas
+%     array_rsa(m) = powermod(find(msj_upper(m) == dicc_let), e, n);
+    array_rsa(m) = powermod(numeros_10(m), d, n);
+    fprintf(id, [num2str(array_rsa(m)),',']);
+end
+fclose(id);
